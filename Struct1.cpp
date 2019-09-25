53769dd98e5ce5ff37cca80018e56aee0b922b49
Jesus <jesusmoramaster@gmail.com>
2019-09-25 02:35:16 -0500

    Primer version de la estructura

---
+++ Struct1.cpp
@@ -0,0 +1,16 @@
+# include <iostream>
+struct  persona {
+char nombre [ 20 ];
+int edad;
+} persona1, persona2;
+int  main ( int argc, char ** argv) {
+    cout<<" dame el nombre de la persona:" <<endl;
+    cin.getline(persona2.nombre , 20 , ' / n ' );
+    cout<<" dame la edad de la persona: "<<endl;
+    cin>>persona2. Edad ;
+    cout<<" el nombre es: "<<persona2.nombre<<endl;
+	cout<<" la edad es; "<<persona2.edad<<endl;
+    system("pause");
+    return  0 ;
+}
+
